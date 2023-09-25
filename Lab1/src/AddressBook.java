40f811548befd7336fa0427b99b6c4437f58d5ba
mahnoor <mahnoorfatima404@gmail.com>
2023-09-25 12:08:44 -0400

    initial draft

--- Lab1/src/AddressBook.java
+++ Lab1/src/AddressBook.java
@@ -2,18 +2,23 @@ import java.util.ArrayList;
 
 public class AddressBook {
 
-    ArrayList<BuddyInfo> buddies;
+    private ArrayList<BuddyInfo> buddies;
 
     public AddressBook() {
-        this.buddies = new ArrayList<BuddyInfo>();
+        buddies = new ArrayList<>();
     }
 
     public void addBuddy(BuddyInfo buddy){
-        buddies.add(buddy);
+        if (buddy != null){
+            buddies.add(buddy);
+        }
     }
 
-    public void removeBuddy(BuddyInfo buddy){
-        buddies.remove(buddy);
+    public BuddyInfo removeBuddy(int index){
+        if (index >= 0 && index < buddies.size()) {
+            return buddies.remove(index);
+        }
+        return null;
     }
 
     public static void main(String[] args){
@@ -21,6 +26,6 @@ public class AddressBook {
         BuddyInfo buddy = new BuddyInfo("Tom", "Carleton", "613");
         AddressBook addressBook = new AddressBook();
         addressBook.addBuddy(buddy);
-        addressBook.removeBuddy(buddy);
+        addressBook.removeBuddy(0);
     }
 }
